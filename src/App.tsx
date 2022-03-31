adbb9acef55ee708ed5cf06d8d77ddcc8a7e0ebc
mishaszu <mishaszu@gmail.com>
2022-03-31 10:59:14 +0200

    Improve UX

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ export enum Mode {
   Clear
 }
 
+const FillP = () => 
+  <p className="w-full py-2 my-2 text-center"></p>
+
 function App() {
   const [graph, setGraph] = useState(new Graph(6, 6))
   const [pathResult, setPathResult] = useState({
@@ -27,6 +30,7 @@ function App() {
     result: dijkstraOutput | null
   })
   const [mode, setMode] = useState(Mode.None)
+  const [loading, setLoading] = useState(false)
 
   const reset = (withGraph = false) => {
     if (withGraph) {
@@ -97,14 +101,18 @@ function App() {
 
   const runDijkstra = () => {
     const start = window.performance.now();
-    dijkstra(graph).then(result => {
-      const end = window.performance.now();
-      const time = end - start;
-      setPathResult({
-        time,
-        result
+    setLoading(true)
+    setTimeout(() => {
+      dijkstra(graph).then(result => {
+        const end = window.performance.now();
+        const time = end - start;
+        setLoading(false)
+        setPathResult({
+          time,
+          result
+        })
       })
-    })
+    }, 50)
   }
 
   const random = () => {
@@ -117,23 +125,45 @@ function App() {
     <>
       <main className="flex items-center w-full h-full align-center">
         <div className="flex flex-col justify-between w-1/4 h-full text-white border-r-4 bg-emerald-600">
-          <h1 className="my-4 text-3xl text-center">Dijkstra algorithm <span className="block">JS/TS</span>implementation</h1>
+          <h1 className="my-4 text-2xl text-center">Dijkstra algorithm <span className="block">JS/TS</span>implementation</h1>
           <div className="flex flex-col items-center">
             <p className="my-2 text-2xl">{graph.width} x {graph.height} grid</p>
             {
               !canRun &&
-              <p className="my-2 text-xl">You have to set start and finish</p>
+              <p className="w-full py-2 text-xl my-2 text-center bg-pink-500">You have to set start and finish</p>
             }
             {
-              pathResult.time &&
-              <p className="my-2">{`Found path in ${pathResult.time}ms`}</p>
+              pathResult.result && pathResult.result.path === null &&
+              <p className="w-full text-xl py-2 my-2 text-center bg-pink-500">Path to finish is blocked!</p>
             }
-            <div>
-              <button onClick={_ => runDijkstra()} className={`px-4 py-2 my-4 mx-2 font-bold text-white rounded cursor-pointer ${canRun ? "bg-blue-500 hover:bg-blue-700" : "bg-gray-500"}`} disabled={!canRun}>Find route</button>
-              <button onClick={_ => reset(true)} className={`px-4 py-2 my-4 font-bold text-white rounded cursor-pointer bg-blue-500 hover:bg-blue-700`}>Reset</button>
-              <button onClick={_ => random()} className={`px-4 py-2 my-4 font-bold text-white rounded cursor-pointer bg-blue-500 hover:bg-blue-700`}>Random</button>
-            </div>
+            {
+              pathResult.time && pathResult.result &&
+              <p className="w-full py-2 my-2 text-xl text-center bg-teal-500">{`Found path in ${pathResult.time}ms on distance ${pathResult.result.distance}`}</p>
+            }
+            {
+              loading &&
+              <p className="w-full text-xl py-2 my-2 text-center bg-violet-500">Calculating path</p>
+            }
+            {
+              !loading && canRun && !pathResult.time && !pathResult.result && 
+              <p className="w-full text-xl py-2 my-2 text-center">Ready to run</p>
+            }
           </div>
+          <form className="flex flex-col items-center p-2 border-t border-b" onSubmit={v => onSubmit(v)}>
+            <h3 className="text-xl">Define new grid dimensions</h3>
+            <div className="flex items-center justify-around">
+              <div className="inline-block w-1/3">
+                <label className="block mb-2 text-sm font-bold text-gray-300" htmlFor="widthInput">Width</label>
+                <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="number" name="widthInput" placeholder="6" />
+              </div>
+              <p>x</p>
+              <div className="inline-block w-1/3 my-4">
+                <label className="block mb-2 text-sm font-bold text-gray-300" htmlFor="heightInput">Height</label>
+                <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="number" name="heightInput" placeholder="6" />
+              </div>
+            </div>
+            <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700" type="submit">Set new grid</button>
+          </form>
           <div className="flex flex-wrap justify-center">
             <ButtonPicker
               onClick={modeClick(Mode.Start)}
@@ -172,21 +202,13 @@ function App() {
               icon={faDumpster}
             />
           </div>
-          <form className="flex flex-col items-center px-8 pt-6 pb-8 mb-4" onSubmit={v => onSubmit(v)}>
-            <h3 className="text-2xl">Define new grid dimensions</h3>
-            <div className="flex items-center justify-around">
-              <div className="inline-block w-1/3">
-                <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="widthInput">Width</label>
-                <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="number" name="widthInput" placeholder="6" />
-              </div>
-              <p>x</p>
-              <div className="inline-block w-1/3 my-4">
-                <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="heightInput">Height</label>
-                <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="number" name="heightInput" placeholder="6" />
-              </div>
+          <div className="flex flex-col items-center my-4">
+            <button onClick={_ => runDijkstra()} className={`px-8 py-4 mx-2 text-2xl font-bold text-white rounded cursor-pointer ${canRun ? "bg-blue-500 hover:bg-blue-700" : "bg-gray-500"}`} disabled={!canRun}>Find route</button>
+            <div>
+              <button onClick={_ => random()} className={`px-4 py-2 my-4 mx-2 font-bold text-white rounded cursor-pointer bg-teal-500 hover:bg-teal-700`}>Random</button>
+              <button onClick={_ => reset(true)} className={`px-4 py-2 my-4 mx-2 font-bold text-white rounded cursor-pointer bg-violet-500 hover:bg-violet-700`}>Reset</button>
             </div>
-            <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700" type="submit">Set new grid</button>
-          </form>
+          </div>
         </div>
         <Grid graph={graph} updateGraph={updateGraph} modeSelected={mode !== Mode.None} result={pathResult.result && pathResult.result?.path ? pathResult.result.path : []} />
       </main>
